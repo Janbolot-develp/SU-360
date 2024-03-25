2d0d11394a801555e4145f377b97e87577f175e9
Janbolot-develp <zumabekovzanbolot942@gmail.com>
2024-03-25 12:39:52 +0600

    SU-360

--- script.js
+++ script.js
@@ -2,21 +2,26 @@ document.body.style.overflow = "hidden"; // Запрет прокрутки ст
 document.body.style.overflow = ""; // Возобновление прокрутки страницы
 
 document.addEventListener("DOMContentLoaded", function () {
-  const letters = document.querySelectorAll(".transition-text span");
+  const urlParams = new URLSearchParams(window.location.search);
+  const skipEffect = urlParams.get('skip_effect');
 
-  // Устанавливаем пользовательский CSS переменной --index для каждой буквы
-  letters.forEach((letter, index) => {
-    letter.style.setProperty("--index", index);
-  });
+  if (!skipEffect) {
+      const letters = document.querySelectorAll(".transition-text span");
 
-  // После завершения анимации букв, скрываем текст и показываем главный контент с задержкой
-  setTimeout(function () {
-    const transitionContainer = document.querySelector(".transition-container");
-    const mainContent = document.querySelector(".main-content");
-    transitionContainer.classList.add("hide");
-    setTimeout(function () {
-      transitionContainer.style.display = "none";
-      mainContent.style.display = "block";
-    }, 500); // Задержка перед показом основного контента (в данном случае 0.5 секунды)
-  }, 2200); // Задержка перед запуском анимации (в данном случае 2.2 секунды)
+      // Устанавливаем пользовательский CSS переменной --index для каждой буквы
+      letters.forEach((letter, index) => {
+          letter.style.setProperty("--index", index);
+      });
+
+      // После завершения анимации букв, скрываем текст и показываем главный контент с задержкой
+      setTimeout(function () {
+          const transitionContainer = document.querySelector(".transition-container");
+          const mainContent = document.querySelector(".main-content");
+          transitionContainer.classList.add("hide");
+          setTimeout(function () {
+              transitionContainer.style.display = "none";
+              mainContent.style.display = "block";
+          }, 500); // Задержка перед показом основного контента (в данном случае 0.5 секунды)
+      }, 2200); // Задержка перед запуском анимации (в данном случае 2.2 секунды)
+  }
 });
